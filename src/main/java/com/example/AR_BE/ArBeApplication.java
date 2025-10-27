44faef6b0388caf66d3d54fc98a102fbc44e0094
tvaexe <vietanh.vadc.2004@gmail.com>
2025-10-28 00:02:23 +0700

    Init Backend

---
+++ src/main/java/com/example/AR_BE/ArBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AR_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArBeApplication.class, args);
+	}
+
+}
